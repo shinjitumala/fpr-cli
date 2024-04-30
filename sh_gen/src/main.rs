ace67d8bb1a8421ae5cb2f6449bdc76e66f61d95
Shinji Hoshino <funny.pig.run@gmail.com>
2024-04-30 10:24:31 +0900

    save.

--- sh_gen/src/main.rs
+++ sh_gen/src/main.rs
@@ -1,6 +1,12 @@
-use std::{env::args, process};
+use std::{
+    env::args,
+    fs::File,
+    io::{BufRead, BufReader},
+    process,
+};
 
 use cli::{cl::*, cl2::*};
+use regex::Regex;
 
 struct C {}
 
@@ -13,6 +19,28 @@ struct Main {
     paths: V<Req<Vec<PathExist>>>,
 }
 
+struct Pats {
+    start: Regex,
+    ty: Regex,
+    arg: Regex,
+    end: Regex,
+}
+
+fn gen(f: &PathExist, p: &Pats) -> String {
+    println!("{:?}", &f.p);
+    let f = BufReader::new(File::open(&f.p).expect("Failed to open file"));
+
+    for l in f.lines() {
+        let l = l.unwrap();
+        if p.start.find(&l).is_some() {
+            println!("start {}", l);
+        }
+        println!("{}", l);
+    }
+
+    format!("")
+}
+
 fn main() {
     let c = C {};
     let a = match cli::cl2::parse::<_, Main>(&c, &args().skip(1).collect::<Vec<_>>()) {
@@ -22,4 +50,17 @@ fn main() {
             process::exit(1);
         }
     };
+
+    let p = Pats {
+        start: Regex::new("^# start metadata$").unwrap(),
+        end: Regex::new("^# end metadata$").unwrap(),
+        ty: Regex::new("^# type text$").unwrap(),
+        arg: Regex::new(r"^([^=])+=\$(\d+) # (.*)$").unwrap(),
+    };
+
+    println!("{:?}", a);
+
+    a.paths.iter().for_each(|f| {
+        gen(f, &p);
+    });
 }
---
+++ sh_gen/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+# start metadata
+# type text
+foo=$1 # description 1
+bar=$2 # description two
+# end metadata
