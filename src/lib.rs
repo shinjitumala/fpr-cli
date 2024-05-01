cc81058fbbdb4e3a01b3b3c57b02b77d0dfd962a
Shinji Hoshino <funny.pig.run@gmail.com>
2024-05-01 20:26:25 +0900

    Added more util.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
-pub mod cl2;
+use prelude::*;
+use std::{env::args, process::exit};
+
 pub mod cl;
+pub mod cl2;
 
 mod test;
 
+pub fn run<Ctx, A: Acts<Ctx>>(c: &Ctx) {
+    match parse::<Ctx, A>(c, &args().skip(1).collect()) {
+        Ok(()) => (),
+        Err(e) => {
+            println!("Error: {e}");
+            exit(1)
+        }
+    }
+}
+
 pub mod prelude {
-    pub use super::cl2::*;
     pub use super::cl::*;
+    pub use super::cl2::*;
+    pub use super::run;
     pub use std::process::Command;
 }
