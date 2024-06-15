9755eff7c0bbc6fc90907fc1f888c67c7303618b
Shinji Hoshino <funny.pig.run@gmail.com>
2024-06-15 23:05:10 +0900

    Changed 'sh' to a separate package. Removed test related files.

--- sh/script/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# start metadata
-# type interactive
-foo=$1 # description 1
-bar=$2 # description two
-# end metadata
-
--- sh/script/test2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# start metadata
-# type text
-foo=$1 # description 1
-bar=$2 # description two
-# end metadata
