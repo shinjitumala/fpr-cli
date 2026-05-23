339b5a7a56581af2f5e77c46d3d9593736035f08
Shinji Hoshino <funny.pig.run@gmail.com>
2026-05-23 21:48:46 +0900

    Fixed trait.

--- src/util.rs
+++ src/util.rs
@@ -261,6 +261,7 @@ impl From<MyErr> for String {
 pub trait Actions: Sized + Clone {
     fn get(prompt: &str, starting_input: Option<&str>) -> Result<Self, MyErr>;
     fn list() -> &'static [&'static str];
+    fn parse(s: &str) -> Option<Self>;
 }
 
 #[derive(Clone)]
